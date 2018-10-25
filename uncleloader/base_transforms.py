636273cde07e79c28a03dd284e0d66b3a4dc5aef
Kevin Xiao <scp173.cool@gmail.com>
2018-10-25 21:20:21 +0800

    update transform

--- uncleloader/base_transforms.py
+++ uncleloader/base_transforms.py
@@ -97,9 +97,47 @@ class ImageResize(object):
                          interpolation=interpolations[self.interp_mode])
         return img, mask
 
+class ImageShorterResize(object):
+    def __init__(self, shorter_length, interp_mode='bilinear'):
+        self.shorter_length = shorter_length
+        self.interp_mode = interp_mode
+
+    def __call__(self, img, mask=None):
+        rows, cols = img.shape[:2]
+        shorter_length = self.shorter_length
+        if rows >= cols:
+            size = (int(shorter_length), int(1.0*rows*shorter_length/cols))
+        else:
+            size = (int(1.0*cols*shorter_length/rows), int(shorter_length))
+        
+        img = cv2.resize(img, size,
+                         interpolation=interpolations[self.interp_mode])
+        if mask is not None:
+            mask = cv2.resize(mask, size, 
+                         interpolation=interpolations[self.interp_mode])
+        return img, mask
+
 """
 AUGUMENTATION FUNCTIONS
 """
+class random_crop(object):
+    def __init__(self, size):
+        self.h, self.w = size
+
+    def __call__(self, img, mask=None):
+        height, width, _ = img.shape
+        assert height >= self.h
+        assert width  >= self.w
+
+        h_start = np.random.randint(0, height - self.h)
+        w_start = np.random.randint(0, width  - self.w)
+        img = img[h_start:h_start+self.h, w_start:w_start+self.h, :]
+        assert img.shape[0] == self.h and img.shape[1] == self.w
+
+        if mask is not None:
+            mask = mask[h_start:h_start+self.h, w_start:w_start+self.h]
+        return img, mask
+
 class random_horizontal_flip(object):
     def __init__(self, prob=0.5):
         self.prob = prob
@@ -145,3 +183,45 @@ class random_transpose(object):
                 mask = mask.transpose(1, 0)
         return img, mask
 
+class random_shear(object):
+    def __init__(self, prob=0.5, range_x=(-0.5, 0.5), range_y=(-0.5, 0.5)):
+        self.prob = prob
+        self.range_x = range_x
+        self.range_y = range_y
+
+    def __call__(self, img, mask=None):
+        if random.random() < self.prob:
+            rows, cols = img.shape[:2]
+            shear_x = np.random.uniform(self.range_x[0], self.range_x[1])
+            shear_y = np.random.uniform(self.range_y[0], self.range_y[1])
+            M = np.array([1, shear_x, 0, 
+                          shear_y, 1, 0, 
+                          0, 0, 1]).reshape((3, 3)).astype(np.float32)
+            img = _apply_perspective(img, M, (rows, cols))
+
+            if mask is not None:
+                mask = _apply_perspective(mask, M, (rows, cols))
+        
+        return img, mask
+
+class random_rescale(object):
+    def __init__(self, prob=0.5, range_x=(0.5, 1.5), range_y=(1, 1)):
+        self.prob = prob
+        self.range_x = range_x
+        self.range_y = range_y
+
+    def __call__(object):
+        if random.random() < self.prob:
+            rows, cols = img.shape[:2]
+            scale_x = np.random.uniform(range_x[0], range_x[1])
+            scale_y = np.random.uniform(range_y[0], range_y[1])
+            M = np.array([scale_x, 0, 0, 
+                          0, scale_y, 0, 
+                          0, 0, 1]).reshape((3, 3)).astype(np.float32)
+            img = _apply_perspective(img, M, (rows, cols))
+
+            if mask is not None:
+                mask = _apply_perspective(mask, M, (rows, cols))
+        
+        return img, mask
+                                  
